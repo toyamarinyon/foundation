394b55bb8db179ab8a41ef16ed2b644ba34fc8cd
toyama-beaver-satoshi <toyamarinyon+beaver@gmail.com>
2014-03-25 18:01:07 +0900

    WIN7: change ln command for windows7

--- init.cmd
+++ init.cmd
@@ -1,4 +1,2 @@
 cd %HOMEPATH%
-fsutil hardlink create _vimrc foundation\vim\vimrc
-mkdir .vim\bundle
-git clone https://github.com/Shougo/neobundle.vim .vim\bundle\neobundle.vim
+mklink _vimrc foundation\vim\vimrc
