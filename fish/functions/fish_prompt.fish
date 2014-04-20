867d36e727d1372862671f19ec99168e8957d259
toyama satoshi <toyamarinyon@gmail.com>
2014-04-20 19:58:37 +0900

    Modify prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -14,9 +14,9 @@ end
 
 function fish_prompt
   if test -d .git
-    printf '%s@%s %s%s%s:%s> ' (whoami) (hostname|cut -d . -f 1) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal) (parse_git_branch)
+    printf '%s%s%s:%s> ' (set_color $fish_color_cwd) (prompt_pwd) (set_color normal) (parse_git_branch)
   else
-    printf '%s@%s %s%s%s> ' (whoami) (hostname|cut -d . -f 1) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+    printf '%s%s%s> ' (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
   end
 end
 
