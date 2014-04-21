8d373b6db6db83cae56c7c372c8fa2c847b3bc5c
toyama satoshi <toyamarinyon@gmail.com>
2014-04-22 08:34:45 +0900

    Modify fish shell config

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,21 @@
-set -x TERM xterm-256color
+# {{{ Basic
+set -export LANG en_US.UTF-8
+set -export TERM xterm-256color
+# }}}
+
+# {{{ Bootstrap Development Tools
+
+# homebrew
+set --export PATH /usr/local/sbin /usr/local/bin $PATH
+
+# rbenv
+if test -f $HOME/.rbenv/bin/rbenv
+  set -export PATH $HOME/.rbenv/bin $PATH
+  status --is-interactive; and . (rbenv init -|psub)
+end
+
+# nvm
+test -s $HOME/.nvm-fish/nvm.fish; and source $HOME/.nvm-fish/nvm.fish
+
+# }}}
+
