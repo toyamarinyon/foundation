9de6f04f884f1baaa3859dcb65665823953f261f
toyama satoshi <toyamarinyon@gmail.com>
2014-05-26 09:27:40 +0000

    Update moromoro

--- fish/config.fish
+++ fish/config.fish
@@ -2,15 +2,17 @@
 set --export LANG en_US.UTF-8
 set --export TERM xterm-256color
 set --export EDITOR vim
+
+test -f $HOME/.config/fish/private_environments.fish; and source $HOME/.config/fish/private_environments.fish
 # }}}
 
 # {{{ Bootstrap Development Tools
 
 # homebrew
-set --export PATH /usr/local/sbin /usr/local/bin $PATH
+set --export PATH /usr/local/sbin /usr/local/bin $HOME/.rbenv/bin $PATH
 
 # rbenv
-which rbenv 2> /dev/null; and status --is-interactive; and source (rbenv init -|psub)
+which rbenv > /dev/null ^&1; and status --is-interactive; and source (rbenv init -|psub)
 
 # nvm
 test -s $HOME/.nvm-fish/nvm.fish; and source $HOME/.nvm-fish/nvm.fish
