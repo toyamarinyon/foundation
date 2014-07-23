6822c84bd914a53e594943b1d4581d8c52b842a1
toyama satoshi <toyamarinyon@gmail.com>
2014-07-23 20:16:28 +0900

    Add zsh aliases for ssh, mosh

--- zsh/zcustom.d/aliases.zsh
+++ zsh/zcustom.d/aliases.zsh
@@ -1,1 +1,3 @@
 alias c='cd $(ghq list -p | peco)'
+alias s='ssh $(grep -iE "^host[[:space:]]+[^*]" ~/.ssh/config|peco|awk "{print \$2}")'
+alias m='ssh $(grep -iE "^host[[:space:]]+[^*]" ~/.ssh/config|peco|awk "{print \$2}")'
