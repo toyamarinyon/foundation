2710333f88c8d3ab26e491b908cee5e61fd5937e
toyama satoshi <toyamarinyon@gmail.com>
2014-09-08 22:45:43 +0900

    Add oepn chrome aliases

--- zsh/zcustom.d/aliases.zsh
+++ zsh/zcustom.d/aliases.zsh
@@ -2,3 +2,12 @@ alias c='cd $(ghq list -p | peco)'
 alias s='ssh $(grep -iE "^host[[:space:]]+[^*]" ~/.ssh/config|peco|awk "{print \$2}")'
 alias m='mosh $(grep -iE "^host[[:space:]]+[^*]" ~/.ssh/config|peco|awk "{print \$2}")'
 alias rm='rm -i'
+
+
+case ${OSTYPE} in
+  darwin*)
+    alias chrome="open /Applications/Google\ Chrome.app"
+    ;;
+  linux*)
+    ;;
+esac
