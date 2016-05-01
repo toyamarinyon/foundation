d951685a0809abe28f40f11819b351a2cd812e2b
toyama satoshi <toyamarinyon@gmail.com>
2016-05-02 07:33:47 +0900

    Update

--- tools/provisioning_vim.sh
+++ tools/provisioning_vim.sh
@@ -1,7 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 
-DEIN_INSTALL_DIR="$HOME/.vim"
+HERE=$(cd $(dirname $0) && pwd)
+VIM_ROOT_DIR="$HOME/.vim"
+DEIN_INSTALL_DIR="$VIM_ROOT_DIR/bundle"
 
 echo -e "\033[32mInstall dein.vim\033[m"
-echo "--> $ curl -fsSL https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh | bash -s $DEIN_INSTALL_DIR"
-curl -fsSL https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh | bash -s $DEIN_INSTALL_DIR
+echo "--> $ curl -fsSL https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh | sh -s $DEIN_INSTALL_DIR"
+curl -fsSL https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh | sh -s $DEIN_INSTALL_DIR
+
+echo ""
+
+echo -e "\033[32mCreate symbolic link plugins.toml\033[m"
+echo "---> $ ln -sf plugins.toml $VIM_ROOT_DIR/plugins.toml"
+ln -sf $HERE/../vim/plugins.toml $VIM_ROOT_DIR/plugins.toml
